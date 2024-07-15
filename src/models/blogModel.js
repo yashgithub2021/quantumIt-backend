c9bf7adacf6bb3608c03127dcac46c6793e20a28
Yash Barge <yashbarge007@gmail.com>
2024-07-15 15:39:34 +0530

    alter blog columns

--- src/models/blogModel.js
+++ src/models/blogModel.js
@@ -3,27 +3,27 @@ const { db } = require("../config/dbConnect"); // Adjust this path
 
 const Blog = db.define("blogs", {
   title: {
-    type: DataTypes.STRING(2048),
+    type: DataTypes.TEXT,
     allowNull: false,
   },
   description: {
-    type: DataTypes.STRING(2048),
+    type: DataTypes.TEXT,
     allowNull: false,
   },
   category: {
-    type: DataTypes.ARRAY(DataTypes.STRING(2048)),
+    type: DataTypes.ARRAY(DataTypes.TEXT),
     allowNull: false,
   },
   image: {
-    type: DataTypes.STRING(2048),
+    type: DataTypes.TEXT,
     allowNull: false,
   },
   image2: {
-    type: DataTypes.STRING(2048),
+    type: DataTypes.TEXT,
     allowNull: false,
   },
   readTime: {
-    type: DataTypes.STRING(2048),
+    type: DataTypes.TEXT,
     allowNull: false,
   },
   detailedInsights: {
@@ -31,27 +31,27 @@ const Blog = db.define("blogs", {
     allowNull: false,
   },
   keyPoints: {
-    type: DataTypes.ARRAY(DataTypes.STRING(2048)),
+    type: DataTypes.ARRAY(DataTypes.TEXT),
     allowNull: false,
   },
   keyInsights: {
-    type: DataTypes.ARRAY(DataTypes.STRING(2048)),
+    type: DataTypes.ARRAY(DataTypes.TEXT),
     allowNull: false,
   },
   quote: {
-    type: DataTypes.STRING(2048),
+    type: DataTypes.TEXT,
     allowNull: false,
   },
   authorName: {
-    type: DataTypes.STRING,
+    type: DataTypes.STRING, // Typically author names don't need unlimited length
     allowNull: false,
   },
   profileImg: {
-    type: DataTypes.STRING,
+    type: DataTypes.STRING, // Typically URLs don't need unlimited length
     allowNull: false,
   },
   designation: {
-    type: DataTypes.STRING(2048),
+    type: DataTypes.TEXT,
     allowNull: false,
   },
   about: {
