4e09135f9d34a12b8b61719787dd9bfa6e89fc33
Amit Hariyale <amithariyale125@gmail.com>
2024-08-29 13:28:47 +0530

    adding edit FAQ API

--- src/controllers/faqController.js
+++ src/controllers/faqController.js
@@ -1,97 +1,131 @@
-const { default: mongoose } = require('mongoose');
-const FaqModel = require('../models/faqModel');
-const catchAsyncError = require('../utils/catchAsyncError');
+const { default: mongoose } = require("mongoose");
+const FaqModel = require("../models/faqModel");
+const catchAsyncError = require("../utils/catchAsyncError");
 const ErrorHandler = require("../utils/ErrorHandler");
 
-
 exports.CreateFaq = async (req, res, next) => {
-    const {
-        question,
-        description,
-        clientName,
-        keyPoints,
-        keyInsights,
-        answer
-    } = req.body;
+  const {
+    question,
+    // description,
+    // clientName,
+    // keyPoints,
+    // keyInsights,
+    answer,
+  } = req.body;
 
-    if (!question || !description || !clientName || !keyPoints || !keyInsights || !answer) {
-        return res.status(400).json({
-            success: false,
-            message: 'Empty Fields'
-        });
-    }
+  if (
+    !question ||
+    // !description ||
+    // !clientName ||
+    // !keyPoints ||
+    // !keyInsights ||
+    !answer
+  ) {
+    return res.status(400).json({
+      success: false,
+      message: "Empty Fields",
+    });
+  }
 
-    try {
-        const faq = await FaqModel.create({
-            question,
-            description,
-            clientName,
-            date: new Date(),
-            keyPoints,
-            keyInsights,
-            answer
-        });
-        res.status(200).json({
-            success: true,
-            message: "Saved Successfully",
-            faq,
-        });
-    } catch (error) {
-        return next(new Error(`Error saving FAQ: ${error.message}`));
-    }
+  try {
+    const faq = await FaqModel.create({
+      question,
+      //   description,
+      //   clientName,
+      date: new Date(),
+      //   keyPoints,
+      //   keyInsights,
+      answer,
+    });
+    res.status(200).json({
+      success: true,
+      message: "Saved Successfully",
+      faq,
+    });
+  } catch (error) {
+    return next(new Error(`Error saving FAQ: ${error.message}`));
+  }
 };
 
 exports.GetFaq = async (req, res, next) => {
-    const { id } = req.query;
+  const { id } = req.query;
 
-    try {
-        if (id) {
-            const faq = await FaqModel.findByPk(id);
-            if (faq) {
-                res.status(200).json({
-                    success: true,
-                    message: "Fetched Successfully",
-                    faq,
-                });
-            } else {
-                res.status(404).json({
-                    success: false,
-                    message: "FAQ not found",
-                });
-            }
-        } else {
-            const faqs = await FaqModel.findAll();
-            res.status(200).json({
-                success: true,
-                message: "Fetched Successfully",
-                faqs,
-            });
-        }
-    } catch (error) {
-        return next(new Error(`Error fetching FAQ: ${error.message}`));
+  try {
+    if (id) {
+      const faq = await FaqModel.findByPk(id);
+      if (faq) {
+        res.status(200).json({
+          success: true,
+          message: "Fetched Successfully",
+          faq,
+        });
+      } else {
+        res.status(404).json({
+          success: false,
+          message: "FAQ not found",
+        });
+      }
+    } else {
+      const faqs = await FaqModel.findAll();
+      res.status(200).json({
+        success: true,
+        message: "Fetched Successfully",
+        faqs,
+      });
     }
+  } catch (error) {
+    return next(new Error(`Error fetching FAQ: ${error.message}`));
+  }
 };
 
-
 exports.DeleteFaq = async (req, res, next) => {
-    const { id } = req.query;
+  const { id } = req.query;
 
-    try {
-        const faq = await FaqModel.findByPk(id);
-        if (!faq) {
-            return res.status(404).json({
-                success: false,
-                message: "FAQ not found",
-            });
-        }
-
-        await faq.destroy();
-
-        res.status(200).json({
-            success: true,
-            message: "Deleted Successfully",
-        });
-    } catch (error) {
-        return next(new Error(`Error deleting FAQ: ${error.message}`));
+  try {
+    const faq = await FaqModel.findByPk(id);
+    if (!faq) {
+      return res.status(404).json({
+        success: false,
+        message: "FAQ not found",
+      });
     }
+
+    await faq.destroy();
+
+    res.status(200).json({
+      success: true,
+      message: "Deleted Successfully",
+    });
+  } catch (error) {
+    return next(new Error(`Error deleting FAQ: ${error.message}`));
+  }
+};
+
+exports.UpdateFaq = async (req, res, next) => {
+  const { id } = req.body.params;
+  const { question, answer } = req.body.body;
+  // console.log("hiiii", { question, answer, id });
+  try {
+    const faq = await FaqModel.findByPk(id);
+    if (!faq) {
+      return res.status(404).json({
+        success: false,
+        message: "FAQ not found",
+      });
+    }
+
+    const updatedFaq = await faq.update({
+      question: question || faq.question,
+      answer: answer || faq.answer,
+    });
+    res.status(200).json({
+      success: true,
+      message: "Updated Successfully",
+      faq: updatedFaq,
+    });
+  } catch (error) {
+    return next(
+      new ErrorHandler(`There was an error updating FAQ: ${error.message}`, 500)
+    );
+  }
 };
--- src/models/faqModel.js
+++ src/models/faqModel.js
@@ -2,34 +2,34 @@ const { Sequelize, DataTypes } = require("sequelize");
 const { db } = require("../config/dbConnect"); // Adjust this path as needed
 
 const FAQ = db.define("faqs", {
-    question: {
-        type: DataTypes.STRING,
-        allowNull: false,
-    },
-    description: {
-        type: DataTypes.TEXT,
-        allowNull: false,
-    },
-    clientName: {
-        type: DataTypes.STRING,
-        allowNull: false,
-    },
-    date: {
-        type: DataTypes.DATE,
-        allowNull: false,
-    },
-    keyPoints: {
-        type: DataTypes.ARRAY(DataTypes.STRING),
-        allowNull: false,
-    },
-    keyInsights: {
-        type: DataTypes.ARRAY(DataTypes.STRING),
-        allowNull: false,
-    },
-    answer: {
-        type: DataTypes.TEXT,
-        allowNull: false,
-    },
+  question: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  // description: {
+  //     type: DataTypes.TEXT,
+  //     allowNull: false,
+  // },
+  // clientName: {
+  //     type: DataTypes.STRING,
+  //     allowNull: false,
+  // },
+  date: {
+    type: DataTypes.DATE,
+    allowNull: false,
+  },
+  // keyPoints: {
+  //     type: DataTypes.ARRAY(DataTypes.STRING),
+  //     allowNull: false,
+  // },
+  // keyInsights: {
+  //     type: DataTypes.ARRAY(DataTypes.STRING),
+  //     allowNull: false,
+  // },
+  answer: {
+    type: DataTypes.TEXT,
+    allowNull: false,
+  },
 });
 
 module.exports = FAQ;
